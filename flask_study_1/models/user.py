67019975ed6d2362cde349439ad5f7322efbc4b3
jsk1107 <seungkyu.korea@gmail.com>
2021-10-15 17:22:04 +0900

    mdl 패키지 적용 추가

--- flask_study_1/models/user.py
+++ flask_study_1/models/user.py
@@ -5,4 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(20), unique=True, nullable=False)
     user_name = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    @classmethod
+    def fine_one_by_user_id(cls, user_id):
+        return User.query.filter_by(user_id=user_id).first()
--- flask_study_1/routes/auth_route.py
+++ flask_study_1/routes/auth_route.py
@@ -1,23 +1,22 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, session, request
+from flask import Blueprint, render_template, redirect, url_for, flash, session, request, g
 from flask_study_1.forms.auth_form import LoginForm, RegisterForm
 from flask_study_1.models.user import User as UserModel
 from werkzeug import security
 
+
 NAME = 'auth'
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
 
-
-""" only for testing """
-class User:
-    def __init__(self, user_id, user_name, password):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.password = password
-
-USERS = []
-USERS.append(User('jsk', 'jsk', security.generate_password_hash('1234')))
-USERS.append(User('admin', 'admin', security.generate_password_hash('1234')))
-USERS.append(User('tester', 'tester', security.generate_password_hash('1234')))
+@bp.before_app_request
+def before_app_request():
+    g.user = None
+    user_id = session.get('user_id')
+    if user_id:
+        user = UserModel.fine_one_by_user_id(user_id)
+        if user:
+            g.user = user
+        else:
+            session.pop('user_id', None)
 
 
 @bp.route('/')
@@ -30,22 +29,14 @@ def login():
     form = LoginForm()
     # POST, validate OK -> 정상적으로 제출이 되었다면?
     if form.validate_on_submit():
-        # TODO
-        #  1) 유저조회
-        #  2) 존재하는 유저인지 확인
-        #  3) Password 정합확인
-        #  4) 로그인 유지(Session)
-
         user_id = form.data.get('user_id')
         password = form.data.get('password')
-
-        user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.fine_one_by_user_id(user_id)
         if user:
-            user = user[0]
             if not security.check_password_hash(user.password, password):
                 flash('Password is not valid.')
             else:
-                session['user_id'] = user_id
+                session['user_id'] = user.user_id
                 return redirect(url_for('base.index'))
         else:
             flash('User Id is not exists.')
@@ -64,18 +55,19 @@ def register():
         password = form.data.get('password')
         repassword = form.data.get('repassword')
 
-        user = [user for user in USERS if user.user_id == user_id]
+        user = UserModel.fine_one_by_user_id(user_id)
         if user:
             flash('User Id is already exists.')
             return redirect(request.path)
         else:
-            USERS.append(
-                User(
+            g.db.add(
+                UserModel(
                     user_id=user_id,
                     user_name=user_name,
                     password=security.generate_password_hash(password)
                 )
             )
+            g.db.commit()
             session['user_id'] = user_id
             return redirect(url_for('base.index'))
     else:
