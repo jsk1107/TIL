1a4326affa8f63d23339babfdf9db42c51644b86
jsk1107 <seungkyu.korea@gmail.com>
2021-10-18 23:12:54 +0900

    config 분리

---
+++ flask_study_1/configs.py
@@ -0,0 +1,30 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+class Config:
+    """ Flask Config """
+    SECRET_KEY = 'secretkey'
+    SESSION_COOKIE_NAME = 'flask_study_1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/flask_study_1?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SWAGGER_UI_DOC_EXPANSION = 'list'
+
+
+class DevelopmentConfig(Config):
+    """ Flask Config for dev """
+    DEBUG = True
+    SEND_FILE_MAX_AGE_DEFAULT = 1
+    # TODO: Front 호출시 처리
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(DevelopmentConfig):
+    pass
+
+
+class TestingConfig(DevelopmentConfig):
+    __test__ = False
+    TESTIN = True
+    # Test시에는 Sqllite를 이용해서 테스트를 한다.
+    SQLALCHEMY_DATABASE_URI = f'sqllite:///{os.path.join(BASE_PATH, "sqllite_test.db")}'
