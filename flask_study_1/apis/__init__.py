35fc465c8e1dec614d9a457fd07aeafec2357e40
jsk1107 <seungkyu.korea@gmail.com>
2021-10-20 23:31:30 +0900

    로그인 유저 api 문서조회 데코레이터 추가

--- flask_study_1/apis/__init__.py
+++ flask_study_1/apis/__init__.py
@@ -1,10 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, g, abort
 from flask_restx import Api
 from .user import ns as UserNamespace
+from .memo import ns as MemoNamespace
+from functools import wraps
+
+
+def check_session(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not g.user:
+            abort(401)
+        return func(*args, **kwargs)
+    return wrapper
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-api = Api(bp, title='Flask Study 1 API', version='1.0', doc='/docs', description='빡공해서 백앤드 정복하자')
+api = Api(bp, title='Flask Study 1 API', version='1.0', doc='/docs', decorators=[check_session],
+          description='빡공해서 백앤드 정복하자')
 
 api.add_namespace(UserNamespace)
+api.add_namespace(MemoNamespace)
