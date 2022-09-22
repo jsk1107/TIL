3083e0574504233fabb53a3ac279d44abd057973
QQQ <mieryu@naver.com>
2022-09-22 21:04:26 +0900

    key 변수 통합

--- VanillaJS/app.js
+++ VanillaJS/app.js
@@ -3,27 +3,28 @@ const loginForm = document.querySelector("#login-form");
 const loginId = document.querySelector("#login-form input:first-child");
 const greeting = document.querySelector("#greeting");
 
-const idKey = "ID";
+const ID_KEY = "ID";
+const HIDDEN_KEY = "hidden"
 
 function onLoginSubmit(event){
     event.preventDefault(); // Submit이후 Page 새로고침 방지
     
     const id = loginId.value;  
-    loginForm.classList.add("hidden");
-    localStorage.setItem(idKey, id);
+    loginForm.classList.add(HIDDEN_KEY);
+    localStorage.setItem(ID_KEY, id);
     paintGreeting(id);
 }
 
 function paintGreeting(isId){
-    greeting.classList.remove('hidden');
+    greeting.classList.remove(HIDDEN_KEY);
     greeting.innerText = `Hello ${isId}`;
 
 }
 
-const isId = localStorage.getItem(idKey);
+const isId = localStorage.getItem(ID_KEY);
 
 if (isId === null){
-    loginForm.classList.remove("hidden");
+    loginForm.classList.remove(HIDDEN_KEY);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else{
     paintGreeting(isId);
