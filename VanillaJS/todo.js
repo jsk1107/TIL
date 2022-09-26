1c071653c22aa8ef6781dd5dd8bbf5fb4ab7f5f3
jsk1107 <seungkyu.korea@gmail.com>
2022-09-26 18:48:10 +0900

    todoList 개발완료

--- VanillaJS/todo.js
+++ VanillaJS/todo.js
@@ -2,23 +2,43 @@ const todoForm = document.querySelector("#todo-form");
 const todo = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
-const list = [];
+let list = [];
 const TODOLIST_KEY = "todoList";
 
-function paintTodo(todoLists){
 
+function paintTodo(oneOfTodoList){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Del";
+    deleteButton.addEventListener("click", deleteTodo);
+    
+    li.innerText = oneOfTodoList.text;
+    li.setAttribute("id", oneOfTodoList.id); 
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+}  
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    list = list.filter((todo) => todo.id != li.id);
+    localStorage.setItem(TODOLIST_KEY, JSON.stringify(list));
+    li.remove();
 }
 
 function addTodoList(event){
     event.preventDefault();
-    const li = document.createElement("li");
-    const tmp = todo.value;
-    list.push(tmp);
-    li.innerText = tmp;
-    todoList.appendChild(li);
-    localStorage.setItem(TODOLIST_KEY, JSON.stringify(list));
+    const newTodo = todo.value;
+    todo.value = "";
+    const newTodoObj = {
+        id: Date.now(),
+        text: newTodo
     }
-
+    list.push(newTodoObj);
+    paintTodo(newTodoObj);
+    localStorage.setItem(TODOLIST_KEY, JSON.stringify(list));
+}
 
 if (isId === null){
     todoForm.classList.add(HIDDEN_KEY);
@@ -29,7 +49,12 @@ if (isId === null){
     const savedTodoList = localStorage.getItem(TODOLIST_KEY);
     if (savedTodoList === null){
         todoForm.addEventListener("submit", addTodoList);
-    } 
+    } else {
+        const parsedTodo = JSON.parse(savedTodoList);
+        parsedTodo.forEach(paintTodo);
+        list = parsedTodo;
+        todoForm.addEventListener("submit", addTodoList);
+    }
     
     
 }
