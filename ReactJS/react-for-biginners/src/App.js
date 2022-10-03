d71757f5a59f15e1fcdd922fb25b97512276a65c
jsk1107 <seungkyu.korea@gmail.com>
2022-10-04 00:55:28 +0900

    5. create-react-app

---
+++ ReactJS/react-for-biginners/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Button text={"Click!"} />
+    </div>
+  );
+}
+
+export default App;
