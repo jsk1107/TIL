b82d9c3dbe0e61be2d063d25f8bdc13e2f910035
jsk1107 <seungkyu.korea@gmail.com>
2022-10-06 20:23:53 +0900

    6. useEffect

--- ReactJS/react-for-biginners/src/App.js
+++ ReactJS/react-for-biginners/src/App.js
@@ -1,11 +1,33 @@
-import Button from "./Button";
 
+import {useState, useEffect} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const [inverse, setInverse] = useState(false);
+
+  const onClick = () => {
+    setCount((current) => current + 1);
+  }
+  const onChange = (event) => {
+    setInput(event.target.value);
+    console.log(input);
+  }
+  const onInverse = () => {
+    setInverse((current) => !current);
+  }
+
+  useEffect(()=>{ console.log("hello", count) }, []);
+  useEffect(()=>{ console.log("hello", input) }, [input]);
+
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Button text={"Click!"} />
+      <h1>{count}</h1>
+      <div>
+        <input value={input} onChange={onChange} type="text" placeholder="write someting text"></input>
+      </div>
+      <button onClick={onClick}> Click </button>
+      <button onClick={onInverse}> {inverse ? "inverse" : "inversed"} </button>
     </div>
   );
 }
