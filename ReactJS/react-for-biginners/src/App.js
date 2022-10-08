b9b8c657e134f34d4e4e8f858139efda0fba98a6
jsk1107 <seungkyu.korea@gmail.com>
2022-10-08 19:50:36 +0900

    practice3_movieApp

--- ReactJS/react-for-biginners/src/App.js
+++ ReactJS/react-for-biginners/src/App.js
@@ -1,58 +1,15 @@
-
-import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App(){
-  const [ loading, setLoading ] = useState(true);
-  const [ coins, setCoins ] = useState([]);
-
-  const [ dollor, setDollor ] = useState();
-  const [ coin, setCoin ] = useState();
-  const [ coinsymbol, setCoinSymbol ] = useState();
-
-  const onUsdChange = (event) => {
-    setDollor(event.target.value);
-  };
-
-  const onCoinChange = (event) => {
-
-
-  }
-
-  const showValue = (event) => {
-    console.log(event);
-    console.log(event.target)
-    console.log(event.target.value);
-  }
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {setCoins(json);
-                     setLoading(false)});
-    
-  }, []);
-
-  console.log(coin, coinsymbol);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : 
-      <select onChange={showValue}>
-        <option value="none">==Select==</option>
-        {coins.map((item) => <option key={item.id}> {item.name}({item.symbol})</option>)}
-      </select>}
-      
-      <div>
-        <form>
-          <input onChange={onUsdChange} value={dollor} type="text" placeholder="USD" disabled={true} />
-          <input onChange={onCoinChange} value={coin} type="text" placeholder={coinsymbol} disabled={true} />
-
-        </form>
-      </div>
-      
-      
-    </div>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
