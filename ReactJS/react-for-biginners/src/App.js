3838ba72c88c4563de9429bebb794da72d994391
jsk1107 <seungkyu.korea@gmail.com>
2022-10-08 17:44:39 +0900

    project2_coin_exchangeUnit_fail

--- ReactJS/react-for-biginners/src/App.js
+++ ReactJS/react-for-biginners/src/App.js
@@ -2,8 +2,27 @@
 import {useState, useEffect} from "react";
 
 function App(){
-  const [ loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+  const [ coins, setCoins ] = useState([]);
+
+  const [ dollor, setDollor ] = useState();
+  const [ coin, setCoin ] = useState();
+  const [ coinsymbol, setCoinSymbol ] = useState();
+
+  const onUsdChange = (event) => {
+    setDollor(event.target.value);
+  };
+
+  const onCoinChange = (event) => {
+
+
+  }
+
+  const showValue = (event) => {
+    console.log(event);
+    console.log(event.target)
+    console.log(event.target.value);
+  }
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -13,13 +32,24 @@ function App(){
     
   }, []);
 
+  console.log(coin, coinsymbol);
   return (
     <div>
-      <h1>The Coins!</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? <strong>Loading...</strong> : 
-      <select>
-        {coins.map((item) => <option key={item.id}> {item.name} {item.symbol} ${item.quotes.USD.price}</option>)}
+      <select onChange={showValue}>
+        <option value="none">==Select==</option>
+        {coins.map((item) => <option key={item.id}> {item.name}({item.symbol})</option>)}
       </select>}
+      
+      <div>
+        <form>
+          <input onChange={onUsdChange} value={dollor} type="text" placeholder="USD" disabled={true} />
+          <input onChange={onCoinChange} value={coin} type="text" placeholder={coinsymbol} disabled={true} />
+
+        </form>
+      </div>
+      
       
     </div>
 
