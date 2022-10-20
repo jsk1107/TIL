e276b09291b8f28a2bd0fe7b1794c1658f1edbf9
jsk1107 <seungkyu.korea@gmail.com>
2022-10-21 01:20:53 +0900

    2.3 'As' and Attrs

--- reactjs-masterclass/src/App.js
+++ reactjs-masterclass/src/App.js
@@ -16,13 +16,30 @@ const Box = styled.div`
 // Box의 모든 스타일을 상속받으려면 styled(Obj)를 사용하면됨.
 const Circle = styled(Box)`
   border-radius: 50px;
-`
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 10px;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  color: white;
+  background-color: tomato;
+`;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="tomato"/>
-      <Circle bgColor="teal"/>
+      <Btn> LogIn</Btn>
+      <Btn as="a" href="/"> LogIn</Btn>
+
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
     </Father>  
   );
 }
