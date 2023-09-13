eda108f28d433831de36ccd84a6b0ca0cb2c77cd
jsk1107 <seungkyu.korea@gmail.com>
2023-09-13 22:07:56 +0900

    ch05_2

--- reactjs-masterclass/src/components/Coins.tsx
+++ reactjs-masterclass/src/components/Coins.tsx
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+    
+`;
+
+const Header = styled.header``;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li``;
+
 const Title = styled.div`
     color: ${props => props.theme.accentColor};
 `
 
+const coin = [
+
+]
 function Coins() {
-    return <Title> Coins </Title>
+    return <Container>
+        <Header>
+            <Title> 코인 </Title>
+        </Header>
+        <CoinsList>
+            <Coin></Coin>
+        </CoinsList>
+    </Container>
 }
 
 export default Coins;
