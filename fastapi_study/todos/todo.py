58af0d7b6a51926b1956833c69caea8a8c0d6d43
jsk1107 <seungkyu.korea@gmail.com>
2024-03-11 23:53:47 +0900

    ch03_exception

--- fastapi_study/todos/todo.py
+++ fastapi_study/todos/todo.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, HTTPException, status
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
-@todo_router.post('/todo')
+@todo_router.post('/todo', status_code=201)
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {"message": "Todo added successfully."}
@@ -19,7 +19,7 @@ async def get_single_todo(todo_id: int=Path(..., title="The ID of the todo to re
     for todo in todo_list:
         if todo.id == todo_id:
             return {'todo': todo}
-    return {"message": "Todo with supplied ID doesn't exist."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo with supplied ID doesn't exist.")
 
 @todo_router.put('/todo/{todo_id}')
 async def update_todo(todo_data: TodoItem, todo_id: int=Path(..., title="The ID of the todo to be updated")) -> dict:
@@ -28,7 +28,7 @@ async def update_todo(todo_data: TodoItem, todo_id: int=Path(..., title="The ID 
         if todo.id != todo_id: continue
         todo.item.item = todo_data
         return {"message": "Todo updated Successfully."}
-    return {"message": "Todo with supplied ID doesn't exist."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo with supplied ID doesn't exist.")
             
 @todo_router.delete('/todo/{todo_id}')
 async def del_todo(todo_id: int=Path(..., title="del todo")) -> dict:
@@ -36,7 +36,7 @@ async def del_todo(todo_id: int=Path(..., title="del todo")) -> dict:
         if todo.id == todo_id:
             todo_list.pop(idx)
             return {"message": "Todo deleted successfully"}
-    return {"message": "Todo with supplied ID doesn't exist."}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo with supplied ID doesn't exist.")
 
 @todo_router.delete('/todo')
 async def del_all_todo():
